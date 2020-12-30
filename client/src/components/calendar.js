6e5e0856a91a210cd70821b2e8dd21e0d988c2a3
sofiacwarren <sofiacwarren@gmail.com>
2020-12-30 16:39:17 -0600

    'trip bars rendering conditionally from dummy data, setting up css for calendar to limit in-line styling'

--- client/src/components/calendar.js
+++ client/src/components/calendar.js
@@ -3,10 +3,17 @@ import { Calendar, Alert } from 'antd';
 import moment from 'moment';
 
 class UserCalendar extends React.Component {
-  state = {
-    value: moment('2020-12-25'),
-    selectedValue: moment('2020-12-31'),
-  };
+  constructor (props) {
+    super (props);
+    this.state = {
+      value: moment(),
+      selectedValue: moment(),
+    };
+    //binding
+    this.onSelect = this.onSelect.bind(this);
+    this.onPanelChange = this.onPanelChange.bind(this);
+    this.dateCellRender = this.dateCellRender.bind(this);
+  }
 
   //selecting a date on calendar
   onSelect = value => {
@@ -22,20 +29,50 @@ class UserCalendar extends React.Component {
 
   //render into each day on calendar
   dateCellRender = value => {
-
+    const dummyData = [{
+      tripId: 2,
+      startDate: '2020-12-04',
+      endDate: '2020-12-08',
+      location: 'Homer, Alaska',
+      type: 'Business'
+      },
+      {
+      tripId: 3,
+      startDate: '2020-12-14',
+      endDate: '2020-12-17',
+      location: 'Springfield, Nebraska',
+      type: 'Business'
+      },
+      {
+      tripId: 4,
+      startDate: '2020-12-24',
+      endDate: '2020-12-31',
+      location: 'Marfa, Texas',
+      type: 'Personal'
+      }
+    ]
+    console.log('value', value.month())
     return (
-      // TODO: map through whatever trip data given to populate calendar conditionally
-      <div
-        className='dayBox'
-        style={{
-          // TODO: background color cycle between business scheme or personal scheme
-          background: 'purple',
-          position: 'absolute',
-          // TODO: style to match outercell width
-          width: '226px'
-        }}
-        // TODO: fill with trip name
-        >trip name</div>
+      <>
+      {dummyData.map((trip) => {
+        if (value.date() >= parseInt(trip.startDate.slice(8)) && value.date() <= parseInt(trip.endDate.slice(8)) && (value.month() + 1) === parseInt(trip.startDate.slice(5,7))) {
+          // console.log('matchmatchmatch')
+          return <div
+            key={trip.tripId}
+            className='tripBox'
+            style={{
+              // TODO: background color cycle between business scheme or personal scheme
+              background: 'violet',
+              position: 'absolute',
+              // styles bar to span width of day element
+              width: 'calc(100% + 8px)',
+              left: '-4px'
+            }}
+            // TODO: fill with trip name/info
+            >{trip.location}</div>
+          }
+          })}
+        </>
     )
   }
 
