dc1b24d617ac979b43f5f3e73b434b1447ed562a
Gledinburgh <gledinburgh@gmail.com>
2020-12-30 20:20:00 -0600

    removed test components

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,12 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Store from './state-management/Store'
-import Test1 from './components/Test1'
-import Test2 from './components/Test2'
-
 
 function App() {
   return (
     <Store>
       <div className="App">
         <Header className="App-header" />
-        <Test1/>
-        <Test2/>
         <Footer />
       </div>
     </Store>
