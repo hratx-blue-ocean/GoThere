06b4dd5b843cb9556d22ecbc4619569c9f350432
Philip Woram <git-commit-address>
2020-12-31 09:07:24 -0800

    Implemented unfolding fan animation for attractions, but still have not fully fixed animation path.

--- client/src/Components/AttractionList/AttractionList.jsx
+++ client/src/Components/AttractionList/AttractionList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Attraction from '../Attraction/Attraction.jsx';
+import './AttractionList.css';
 
 export default function AttractionList(props) {
-	const cssClasses = ['List', props.show ? 'ListOpen' : 'ListClosed'];
+	// const cssClasses = ['List', props.show ? 'ListOpen' : 'ListClosed'];
 	return (
-		<div className={cssClasses.join(' ')}>
+		<div className="ListOpen">
 			<Attraction />
 			<Attraction />
 			<Attraction />
