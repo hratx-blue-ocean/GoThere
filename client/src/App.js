3466adf4ae07b7bdcd108c64421599e19e61c97a
Gledinburgh <gledinburgh@gmail.com>
2020-12-30 19:54:34 -0600

    Included Test2 component in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Store from './state-management/Store'
 import Test1 from './components/Test1'
+import Test2 from './components/Test2'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Header className="App-header" />
         <Test1/>
+        <Test2/>
         <Footer />
       </div>
     </Store>
