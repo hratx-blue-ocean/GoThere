6a1e3557808c5984f9e71a206132374d2fd3b05c
Gledinburgh <gledinburgh@gmail.com>
2021-01-01 21:18:43 -0600

    date selecter no prevents users from selecting dates that before the selected end date or current date

--- client/src/components/TravelBar/TravelBar.jsx
+++ client/src/components/TravelBar/TravelBar.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useContext, useState} from 'react';
+import moment from 'moment';
 import {Context} from '../../state-management/Store';
 import TripInfoObject from './TripInfoObject.js';
 import './TravelBar.css';
@@ -10,14 +11,22 @@ import {DatePicker} from 'antd';
 const TravelBar = () => {
 
   var [state, dispatch] = useContext(Context);
-
   var [tripInfo, setTripInfo] = useState({...TripInfoObject})
 
+  function disabledDate(current) {
+
+    let customDate = state.tripInfo.endDate;
+    if (customDate) {
+      return (current && current > moment(customDate, "YYYY-MM-DD")) || (current && current < moment().endOf('day'));
+    }
+    // if (state.tripInfo.endDate) {console.log('endDate', state.tripInfo.endDate)}
+    // return current && current < moment().endOf('day');
+  }
+
   const handleTripInfoChange = (date, dateString, name ) => {
-  var tripInfoClone = {...tripInfo};
-  tripInfoClone[name] = dateString;
-  console.log('trip info', tripInfoClone);
-  setTripInfo(tripInfoClone);
+    var tripInfoClone = {...tripInfo};
+    tripInfoClone[name] = dateString;
+    setTripInfo(tripInfoClone);
   }
 
   const updateGlobalState = () => {
@@ -26,12 +35,16 @@ const TravelBar = () => {
 
  useEffect(() => {
    updateGlobalState()
+   console.log('re-render')
   },[tripInfo]);
 
    return (
     <div >
-      <DatePicker name="startDate" placeholder="Start" onChange={(date, dateSting) => handleTripInfoChange(date, dateSting, 'startDate')}/>
-      <DatePicker name="endDate"   placeholder="End"   onChange={(date, dateSting) => handleTripInfoChange(date, dateSting, 'endDate')}/>
+      <DatePicker format="YYYY-MM-DD" name="startDate" placeholder="Start"
+        onChange={(date, dateSting) => handleTripInfoChange(date, dateSting, 'startDate')}
+        disabledDate={disabledDate}/>
+      <DatePicker format="YYYY-MM-DD" name="endDate" placeholder="End"
+        onChange={(date, dateSting) => handleTripInfoChange(date, dateSting, 'endDate')}/>
       <SaveTrip/>
     </div>
   )
