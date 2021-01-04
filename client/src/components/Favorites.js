4b3103caf68120e8fe32a2aaed8148e172931a78
Stephanie Green <steph.green44@gmail.com>
2021-01-04 10:32:42 -0700

    add list components

---
+++ client/src/components/Favorites.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Card, List } from "antd";
+
+const tabList = [
+  {
+    key: "Hotels",
+    tab: "Hotels",
+  },
+  {
+    key: "Restaurants",
+    tab: "Restaurants",
+  },
+];
+
+const data = [
+  "Holiday Inn Express, Austin TX",
+  "Marriot City Center, Seattle WA",
+  "Comfort Inn, Orlando FL",
+];
+
+// const data = [{
+//   Date: "12 Jan - 15 Jan 2021",
+//   Hotel: "Marriot City Center",
+//   Weather: "Sunny 84",
+//   Attractions: "Escape Room",
+// }];
+
+const contentList = {
+  Hotels: (
+    <List
+      size="small"
+      dataSource={data}
+      renderItem={(item) => <List.Item>{item}</List.Item>}
+    />
+  ),
+  Restaurants: <p>content2</p>,
+};
+
+class Favorites extends React.Component {
+  state = {
+    key: "Hotels",
+  };
+
+  onTabChange = (key, type) => {
+    console.log(key, type);
+    this.setState({ [type]: key });
+  };
+
+  render() {
+    return (
+      <>
+        <Card
+          style={{ maxWidth: "50%" }}
+          title="Favorites"
+          tabList={tabList}
+          activeTabKey={this.state.key}
+          onTabChange={(key) => {
+            this.onTabChange(key, "key");
+          }}
+        >
+          {contentList[this.state.key]}
+        </Card>
+      </>
+    );
+  }
+}
+
+export default Favorites;
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import TabsCard from "../../components/TabsCard";
+import Favorites from "../../components/Favorites";
+import PastTrips from "../../components/PastTrips";
 
 export default function Dashboard() {
   return (
-    <h2>Dashboard (placeholder)</h2>
+    <React.Fragment>
+      <h2>Dashboard (placeholder)</h2>
+      <TabsCard />
+      <Favorites />
+      <PastTrips />
+    </React.Fragment>
   );
 }
