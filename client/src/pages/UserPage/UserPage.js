90348d1797f43339d90bf9c221483825752d3e47
sofiacwarren <sofiacwarren@gmail.com>
2021-01-04 16:41:41 -0600

    'added components to user page, minimal styling to render correctly'

--- client/src/pages/UserPage/UserPage.js
+++ client/src/pages/UserPage/UserPage.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import UserCalendar from '../../components/Calendar/calendar.js'
+import './userPage.css';
+import UserCalendar from '../../components/Calendar/calendar.js';
+import TabsCard from '../../components/TabsCard.js';
+import Favorites from '../../components/Favorites.js';
+import PastTrips from '../../components/PastTrips';
 
 export default function UserPage() {
   return (
-    <>
-    <h2>User Page (placeholder)</h2>
-    <UserCalendar/>
-    </>
+    <div className="userPage">
+      <div className="calendar">
+        <UserCalendar/>
+      </div>
+      <div className="usertabs">
+        <TabsCard/>
+        <Favorites/>
+        <PastTrips/>
+      </div>
+    </div>
   );
 }
