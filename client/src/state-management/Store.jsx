587f929592a55774f76d52fb5f68685c66f8c8a1
Philip Woram <git-commit-address>
2021-01-04 10:29:26 -0800

    Merged with pre-production

--- client/src/state-management/Store.jsx
+++ client/src/state-management/Store.jsx
@@ -1,17 +1,10 @@
 import React, { createContext, useReducer } from 'react';
 import Reducer from './Reducer';
 
-<<<<<<< HEAD
-const initialState = {
-	test: [],
+var initialState = {
+	tripInfo: {},
 	test2: [],
 	error: null,
-=======
-var initialState = {
-  tripInfo: {},
-  test2: [],
-  error: null
->>>>>>> pre-production
 };
 
 const Store = ({ children }) => {
