2164da6caabe32c747c8817c393c9acac7977533
Jesse Perez <jessep1130@gmail.com>
2021-01-03 23:43:41 -0600

    final commit for the night

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-    type Query {
-      hello: String
-    }
-  `);
+  type Query {
+    hello: String
+  }
+`);
 
 // The root provides a resolver function for each API endpoint
 var root = {
