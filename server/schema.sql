f86b6d625b94b469157abcb12ff2f6bb77a717f2
Jesse Perez <jessep1130@gmail.com>
2021-01-05 11:38:51 -0600

    created user table and added 2 fake users

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,20 @@
-CREATE DATABASE gothere
+DROP DATABASE IF EXISTS gothere;
+CREATE DATABASE gothere;
+\c gothere;
 
 CREATE TABLE users (
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
-  address VARCHAR(),
-  city VARCHAR(),
-  state VARCHAR(),
-  email VARCHAR(),
-  password VARCHAR(),
-  favorites VARCHAR()
-)
+  address VARCHAR,
+  city VARCHAR,
+  state VARCHAR,
+  email VARCHAR,
+  password VARCHAR,
+  favorites VARCHAR
+);
+
+INSERT INTO users(name, address, city, state, email, password, favorites)
+VALUES ('Test User', '1234 gothere St', 'Austin', 'TX', 'gothere@fake.com', 'password123', 2);
+
+INSERT INTO users(name, address, city, state, email, password, favorites)
+VALUES ('Jesse Perez', '4321 gothere St', 'Austin', 'TX', 'jessep@fake.com', 'password123', 2);
