b41073c9040806dd64cae6d60269b2eabde19b99
Gledinburgh <gledinburgh@gmail.com>
2021-01-06 09:58:11 -0600

    changing branches

--- client/src/components/TravelBar/TravelBarOptions.jsx
+++ client/src/components/TravelBar/TravelBarOptions.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useContext, useState} from 'react';
 import {Context} from '../../state-management/Store';
-import TripInfoObject from './TripInfoObject.js';
 import {Rate, Dropdown, Button, Menu, Radio } from 'antd';
 
 
@@ -11,13 +10,20 @@ const TravelBarOptions = () => {
 
   const handleTripInfoChange = (event) => {
     var selectedType = event.target.value;
+    var tripInfoClone = {...state.tripInfo}
+    tripInfoClone.type = selectedType;
+    dispatch({type: 'SET_TRIP_INFO', payload: tripInfoClone});
     setTripType(selectedType);
+
   }
 
   const menu = (
     <Menu>
       <Menu.Item>
-        <div>preferrd hotel rating<Rate/></div>
+        <div>preferrd hotel rating
+          <Rate allowHalf defaultValue={2.5}
+          onChange={handleTripInfoChange}
+          /></div>
       </Menu.Item>
       <Menu.Item>
         <Radio.Group onChange={handleTripInfoChange} value={tripType}>
--- client/src/dummy-data/dummyFavoritesArray.js
+++ client/src/dummy-data/dummyFavoritesArray.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-const dummyFavoritesArray = ["EKX0N6LCJW9XmiOCJFjPPQ"]
-=======
 const dummyFavoritesArray = ["EKX0N6LCJW9XmiOCJFjPPQ", "AkxajgbDyzswEUQvT60UrQ"];
 
 export default dummyFavoritesArray;
->>>>>>> f848388... began adding dummy data
