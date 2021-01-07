65912fc1929537f5c976074d63099cd1bed3d09e
Stephanie Green <steph.green44@gmail.com>
2021-01-07 07:29:28 -0700

    remove unused import

--- client/src/components/ProfileLists/UpcomingTrips.js
+++ client/src/components/ProfileLists/UpcomingTrips.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../../state-management/Store";
 
 import { Card, List } from "antd";
