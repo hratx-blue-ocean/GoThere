a3f7dcdb5937e5183e84142a354fb39ba222775d
Stephanie Green <steph.green44@gmail.com>
2021-01-07 15:10:07 -0700

    refactor with global data store

--- client/src/components/ProfileLists/UpcomingTrips.js
+++ client/src/components/ProfileLists/UpcomingTrips.js
@@ -1,70 +1,67 @@
 import React, { useContext, useState } from "react";
+import moment from "moment";
 import { Context } from "../../state-management/Store";
-
 import { Card, List } from "antd";
 
-const tabList = [
+const today = moment().format("YYYY-MM-DD").toString();
+console.log(today);
+
+const newTrip = [
   {
-    key: "Seattle",
-    tab: "Seattle",
+    id: 12345,
+    valid: true,
+    location: {
+      city: "Memphis",
+      state: "TN",
+      adress: undefined,
+      zip: undefined,
+    },
+    startDate: "2021-01-15",
+    endDate: "2021-01-20",
+    type: "business",
   },
   {
-    key: "Orlando",
-    tab: "Orlando",
+    id: 6789,
+    valid: true,
+    location: {
+      city: "Tampa",
+      state: "FL",
+      adress: undefined,
+      zip: undefined,
+    },
+    startDate: "2021-01-01",
+    endDate: "2021-01-03",
+    type: "personal",
   },
 ];
-
-const data = [
-  "Date: 12 Jan - 15 Jan 2021",
-  "Hotel: Marriot City Center",
-  "Weather: Sunny 84",
-  "Attractions: Escape Room",
-];
-
-const contentList = {
-  Seattle: (
-    <List
-      size="small"
-      dataSource={data}
-      renderItem={(item) => <List.Item>{item}</List.Item>}
-    />
-  ),
-  Orlando: (
-    <List
-      size="small"
-      dataSource={data}
-      renderItem={(item) => <List.Item>{item}</List.Item>}
-    />
-  ),
-};
 
 const UpcomingTrips = () => {
   const [state, dispatch] = useContext(Context);
-  const [userTrips, updateTrip] = useState("blank");
-  const [tabKey, setTabKey] = useState("Seattle");
-
-  const onTabChange = (key, type) => {
-    console.log(state.tripInfo);
-    updateTrip("example trip data"); // updates local trip state
-    console.log(userTrips);
-    dispatch({ type: "tripInfo", payload: "UPDATE STORE" }); // how to update Store tripInfo state?
-    console.log(state.tripInfo);
-    setTabKey(key);
-  };
+  const [userTrips, updateTrip] = useState(newTrip); // use as temp state, switch to global store
 
   return (
     <>
-      <Card
-        className="profileCard"
-        title="Upcoming Trips"
-        tabList={tabList}
-        activeTabKey={tabKey}
-        onTabChange={(key) => {
-          onTabChange(key, "key");
-        }}
-      >
-        {contentList[tabKey]}
-      </Card>
+      <h2>Upcoming Trips</h2>
+      {newTrip
+        .filter((current) => current.startDate >= "2021-01-07") //TODO use the current date #hardcoded
+        .map((trip) => {
+          return (
+            <Card
+              // style={{ maxWidth: "50%" }}
+              title={trip.location.city}
+              extra={<a href="/home">Update Trip</a>}
+            >
+              <List size="small">
+                <List.Item>
+                  Location: {trip.location.city}, {trip.location.state}
+                </List.Item>
+                <List.Item>Arrival Date: {trip.startDate}</List.Item>
+                <List.Item>Departure Date: {trip.endDate}</List.Item>
+                <List.Item>Hotel: </List.Item>
+              </List>
+            </Card>
+          );
+        })}
     </>
   );
 };
