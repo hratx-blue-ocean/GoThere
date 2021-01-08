e6a032dcdc0060fe120a4ecf55e53aed96853d06
Philip Woram <git-commit-address>
2021-01-08 14:26:27 -0800

    Readded Attractions Info

---
+++ client/src/components/AttractionsInfo/AttractionsInfo.jsx
@@ -0,0 +1,15 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '../../state-management/Store';
+import AttractionsBar from '../../components/AttractionsBar/AttractionsBar';
+
+export default function AttractionsInfo() {
+  const [state, dispatch] = useContext(Context);
+
+
+
+  return (
+    <div>
+      <AttractionsBar location={state.tripInfo.location}/>
+    </div>
+  )
+}
