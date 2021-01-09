ea735b35dee78a826accf79eae19770162cc5dc9
sofiacwarren <sofiacwarren@gmail.com>
2021-01-08 22:47:50 -0600

    fixed app import path

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Preferences from './pages/Preferences/Preferences';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import LogOut from './components/SignOut/LogOut';
+import LogOut from './components/SignOut/Logout';
 import Store from './state-management/Store';
 
 function App() {
