1ab9b0cc76dcc959bf1be24ea5a911b33fd721ca
Stephanie Green <steph.green44@gmail.com>
2021-01-09 09:48:14 -0700

    udpate upcoming trips

--- client/src/components/ProfileLists/UpcomingTrips.js
+++ client/src/components/ProfileLists/UpcomingTrips.js
@@ -6,6 +6,20 @@ import { Card, List } from "antd";
 const today = moment().format("YYYY-MM-DD").toString();
 console.log(today);
 
+const TripInfoObject = {
+  id: null,
+  valid: false,
+  location: undefined,
+  city: undefined,
+  state: undefined,
+  address: undefined,
+  zip: undefined,
+  startDate: undefined,
+  endDate: undefined,
+  type: undefined,
+  attractions: [],
+};
+
 const newTrip = [
   {
     id: 12345,
@@ -39,22 +53,25 @@ const UpcomingTrips = () => {
   const [state, dispatch] = useContext(Context);
   const [userTrips, updateTrip] = useState(newTrip); // use as temp state, switch to global store
 
+  // if there are no saved trips, the component will not render
+  // if (state.TripInfoObject.location === undefined) {
+  //   return null;
+  // }
+
   return (
     <>
       <h2>Upcoming Trips</h2>
-      {newTrip
+      {state.trips
         .filter((current) => current.startDate >= "2021-01-07") //TODO use the current date #hardcoded
         .map((trip) => {
           return (
             <Card
               // style={{ maxWidth: "50%" }}
-              title={trip.location.city}
-              extra={<a href="/home">Update Trip</a>}
+              title={trip.location}
+              extra={<a href="/home">Update Trip</a>} // get page to load with TripInfoObject data filled into the circles
             >
               <List size="small">
-                <List.Item>
-                  Location: {trip.location.city}, {trip.location.state}
-                </List.Item>
+                <List.Item>Location: {trip.location}</List.Item>
                 <List.Item>Arrival Date: {trip.startDate}</List.Item>
                 <List.Item>Departure Date: {trip.endDate}</List.Item>
                 <List.Item>Hotel: </List.Item>
