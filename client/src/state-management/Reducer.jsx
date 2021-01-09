28422950144cdf7fa2c71cf62bb0573173ccc0b2
Glenn Edinburgh <Gledinburgh@gmail.com>
2021-01-09 16:22:06 -0600

    Update Reducer.jsx
    resolved merge conflict

--- client/src/state-management/Reducer.jsx
+++ client/src/state-management/Reducer.jsx
@@ -15,7 +15,7 @@ const Reducer = (state, action) => {
 				...state,
 				error: action.payload,
 			};
-<<<<<<< HEAD
+
 		case 'SET_USER':
 			return {
 				...state,
@@ -26,13 +26,13 @@ const Reducer = (state, action) => {
 				...state,
 				email: action.payload,
 			};
-=======
+
 			case 'SET_SAVED_ATTRACTION':
 				return {
 					...state,
 					tripInfo: action.payload,
 				};
->>>>>>> fd87cba9bd1232e2c46d60b6e7ddafff3557031e
+
 		default:
 			return state;
 	}
