2dcf23ccc8b1a1d07fb282e7231e9816077ece72
Stephanie Green <steph.green44@gmail.com>
2021-01-11 06:20:24 -0700

    update favs lists

--- client/src/components/ProfileLists/Favorites.js
+++ client/src/components/ProfileLists/Favorites.js
@@ -10,23 +10,69 @@ const tabList = [
     key: "Restaurants",
     tab: "Restaurants",
   },
+  {
+    key: "Bars",
+    tab: "Bars",
+  },
+  {
+    key: "Shopping",
+    tab: "Shopping",
+  },
 ];
 
-const data = [
-  "Holiday Inn Express, Austin TX",
+const favoriteHotels = [
+  "Hotel San Jose, Austin TX",
   "Marriot City Center, Seattle WA",
   "Comfort Inn, Orlando FL",
+];
+
+const favoriteRestaurants = [
+  "Bird Bird Biscuit, Austin TX",
+  "Cafe Munir, Seattle WA",
+  "Se7en Bites, Orlando FL",
+];
+
+const favoriteBars = [
+  "Edge Rooftop + Bar, Austin TX",
+  "Tanks, Seattle WA",
+  "Whollotta Colada, Orlando FL",
+];
+
+const favoriteShopping = [
+  "Hill Country Galleria, Austin TX",
+  "Oceanside Marina, Seattle WA",
+  "Greenhill Outlets, Orlando FL",
 ];
 
 const contentList = {
   Hotels: (
     <List
       size="small"
-      dataSource={data}
+      dataSource={favoriteHotels}
       renderItem={(item) => <List.Item>{item}</List.Item>}
     />
   ),
-  Restaurants: <p>content2</p>,
+  Restaurants: (
+    <List
+      size="small"
+      dataSource={favoriteRestaurants}
+      renderItem={(item) => <List.Item>{item}</List.Item>}
+    />
+  ),
+  Bars: (
+    <List
+      size="small"
+      dataSource={favoriteBars}
+      renderItem={(item) => <List.Item>{item}</List.Item>}
+    />
+  ),
+  Shopping: (
+    <List
+      size="small"
+      dataSource={favoriteShopping}
+      renderItem={(item) => <List.Item>{item}</List.Item>}
+    />
+  ),
 };
 
 class Favorites extends React.Component {
